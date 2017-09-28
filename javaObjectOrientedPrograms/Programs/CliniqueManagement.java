43a33fe152926467815105b8955d713f9716c3a2
HemanthKasireddy <31645181+HemanthKasireddy@users.noreply.github.com>
2017-09-28 15:30:32 +0530

    Add files via upload

---
+++ javaObjectOrientedPrograms/Programs/CliniqueManagement.java
@@ -0,0 +1,11 @@
+package com.bridgeit.javaObjectOrientedPrograms.Programs;
+
+import com.bridgeit.javaObjectOrientedPrograms.Utility.Utility;
+
+public class CliniqueManagement {
+
+	public static void main(String[] args) {
+		Utility.doctorsInformation();
+	}
+
+}
